efbc5ba501e918afb8dfd49e9b13f838eb09dc67
fernando <crosio.fernando@gmail.com>
2022-09-29 15:34:41 -0300

    Se recorren los data.json de los pokemones y se los recorre con una función map sin hacer uso de un forEach cargandoi un arrayNombres

--- app.js
+++ app.js
@@ -26,9 +26,9 @@ const devPokemones = async() => {
             const res = await fetch ('https://pokeapi.co/api/v2/pokemon/')
             const data = await res.json()
             console.log(data.results)
-            data.results.forEach(element => {
-                console.log(element.name)
-            })
+            const arrayNombres= data.results.map (poke => poke.name) 
+                console.log(arrayNombres)
+            
 
 
         }catch(error){

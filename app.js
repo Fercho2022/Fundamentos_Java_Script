52d21ed1b7d98182cc177dc12b35deb7ec5c66ad
fernando <crosio.fernando@gmail.com>
2022-10-04 14:40:07 -0300

    cambiamos el puerto otra vez6

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 const app= express();
 
-const port =Process.env.PORT || 3000;
+const port =process.env.PORT || 3000;
 
 //motor de plantilla
 app.set("view engine", "ejs");

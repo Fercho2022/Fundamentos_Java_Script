54e348d192d3acd21e62a5d5b7bb7d2f1ed4593f
fernando <crosio.fernando@gmail.com>
2022-10-14 20:38:33 -0300

    Leyendo datos desde mongoDB mediante mongoose

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-
+// modulo de servidor express
 const express = require("express");
-
+const bodyParser=require ('body-parser')
 const app= express();
+
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 
 require('dotenv').config();
 

1a511c220dd5350a51b8be370cc4d4b5ca11aeee
fernando <crosio.fernando@gmail.com>
2022-10-19 10:16:53 -0300

    update de mascota utilizando fetch y body mediante la creacion de la ruta con put

--- router/Mascotas.js
+++ router/Mascotas.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const Mascota=require('../models/mascota')
+const bodyParser=require ('body-parser')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 
 
 
@@ -50,6 +56,47 @@ router.post('/', async (req, res) =>{
         
 })
 
+router.get('/:id', async (req, res) => {
 
+        const id = req.params.id
+
+        try{
+                const mascotaDB= await Mascota.findOne({_id: id});
+                console.log(mascotaDB)
+                res.render('detalle', {mascota: mascotaDB, error:false})
+                
+        }catch(error){
+
+                console.log(error)
+                res.render('detalle', {error: true, mensaje: 'Nose encuentra el id seleccionado'})
+        }
+})
+
+router.put('/:id', async(req, res) =>{
+
+        const id = req.params.id
+        const body =req.body
+        try {
+                                
+                const mascotaDB=await Mascota.findByIdAndUpdate(id, body, { useFindAndModify: false  })
+                console.log(mascotaDB)
+                res.json({
+
+                     estado: true,
+                     mensaje:'Editado'
+
+                })
+        }catch (error){
+
+                console.log(error)
+
+                res.json({
+                        estado: false,
+                        mensaje: 'Fallamos'
+
+               })
+        }
+
+})
 
 module.exports = router;
